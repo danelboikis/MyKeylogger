521848816976bb9e5beb66d8275997fe56e8ceb1
danelbo <danelbo@post.bgu.ac.il>
2024-02-01 12:43:27 +0200

    a

--- app/src/main/java/com/example/mykeylogger/MainActivity.java
+++ app/src/main/java/com/example/mykeylogger/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.mykeylogger;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button startBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startBtn = findViewById(R.id.startBtn);
+
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS));
+            }
+        });
     }
 }
